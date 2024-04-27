ba8c36dffc434d9320dd3a868ee65524b7ab6ddd
AverageEnjoyer2 <vsevolod.t.0320@gmail.com>
2024-04-28 01:30:04 +0300

    Добавлена новая игра!

--- main.py
+++ main.py
@@ -9,13 +9,10 @@ from random import choice
 API_TOKEN = "7073227849:AAHQ85zGxsC8Jv4kc27ec7NTCFsAR0p0MiA"
 
 logging.basicConfig(level=logging.INFO)
-cities = [["Армавир", "Краснодарский Край"], ["Сальск", "Ростовская область"], ["Обнинск", "Калужская область"],
-          ["Ачинск", "Красноярский край"]]
-
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 waiting_for_answer = False
-city = []
+rightansw = []
 proposed_answers = []
 chat_id = ""
 POSTGRES_CONN = "postgres://postgres:S11032008@127rrr.0.0.1:8080/prodtest"
@@ -71,12 +68,12 @@ async def start_message(message: types.Message):
 
 @dp.message_handler(commands=['play'])
 async def start_message(message: types.Message):
-    global city, cities
+    global rightansw
     cities = [["Армавир", "Краснодарский Край"], ["Сальск", "Ростовская область"], ["Обнинск", "Калужская область"],
               ["Ачинск", "Красноярский край"]]
-    city = choice(cities)
+    rightansw = choice(cities)
     geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode=" \
-                       f"{city[0]}&kind=metro&format=json"
+                       f"{rightansw[0]}&kind=metro&format=json"
     response = requests.get(geocoder_request)
     json_response = response.json()
     coords = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"][
@@ -87,9 +84,9 @@ async def start_message(message: types.Message):
     with open(map_file, "wb") as file:
         file.write(response.content)
     global proposed_answers
-    proposed_answers = [city[1]]
+    proposed_answers = [rightansw[1]]
     testcities = cities
-    del testcities[testcities.index(city)]
+    del testcities[testcities.index(rightansw)]
     for i in range(3):
         proposed_answers.append(testcities.pop(testcities.index(choice(testcities)))[1])
     inline_kb = InlineKeyboardMarkup(row_width=2)
@@ -110,7 +107,7 @@ async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
     global waiting_for_answer
     if waiting_for_answer:
         code = int(callback_query.data[-1])
-        if proposed_answers[code - 1] == city[1]:
+        if proposed_answers[code - 1] == rightansw[1]:
             cur = connection.cursor()
             cur.execute(f"""SELECT wins FROM useripstats WHERE userip = '{callback_query.from_user.id}'""")
             result = cur.fetchall()
@@ -124,7 +121,7 @@ async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
             cur.execute(
                 f"""UPDATE useripstats SET losses = '{int(result[0][0]) + 1}' WHERE userip = '{callback_query.from_user.id}'""")
             connection.commit()
-            await bot.send_message(chat_id=chat_id, text=f'Неправильно! Правильный субъект - {city[1]}')
+            await bot.send_message(chat_id=chat_id, text=f'Неправильно! Правильный субъект - {rightansw[1]}')
         waiting_for_answer = False
 
 
@@ -139,6 +136,31 @@ async def start_message(message: types.Message):
         await message.answer(f'Похожы вы ещё ни разу не проигрывали! Число ваших побед - {result[0][0]}')
     else:
         await message.answer(f'Число ваших побед - {result[0][0]}, Число ваших проигрышей - {result[0][1]}, соотношение побед к проигрышам - {round(int(result[0][0]) / int(result[0][0]))}')
+
+
+@dp.message_handler(commands=['playflag'])
+async def start_message(message: types.Message):
+    global rightansw
+    countries = [["Guat.png", "Гватемала"], ["CAR.png", "Центральноафриканская республика"],
+                 ["Switzerland.png", "Швейцария"], ["Bolivia.png", "Боливия"]]
+    rightansw = choice(countries)
+    global proposed_answers
+    proposed_answers = [rightansw[1]]
+    testcities = countries
+    del testcities[testcities.index(rightansw)]
+    for i in range(3):
+        proposed_answers.append(testcities.pop(testcities.index(choice(testcities)))[1])
+    inline_kb = InlineKeyboardMarkup(row_width=2)
+    i = 0
+    for propregion in proposed_answers:
+        i += 1
+        inline_kb.add(InlineKeyboardButton(propregion, callback_data=f'btn{i}'))
+    global waiting_for_answer
+    waiting_for_answer = True
+    global chat_id
+    chat_id = message.chat.id
+    with open(rightansw[0], 'rb') as photo:
+        await bot.send_photo(chat_id=message.chat.id, photo=photo, reply_markup=inline_kb)
 
 
 if __name__ == '__main__':
